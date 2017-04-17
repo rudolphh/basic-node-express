4d1187695977683051d5b9bfcda122bf3f5294b3
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-04-17 12:50:36 -0700

    implement a root-level request logger middleware

--- myApp.js
+++ myApp.js
@@ -7,7 +7,10 @@ var dotenv = require('dotenv');
 dotenv.load();
 
 // --> 7)  Mount the Logger middleware here
-
+app.use(function(req, res, next){
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
 
