f290b3765e2fbbd5fd73720c87635c7e0a391c7c
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-04-18 00:34:45 -0700

    get query parameter input from the client

--- myApp.js
+++ myApp.js
@@ -69,7 +69,9 @@ app.get('/:word/echo', function(req, res){
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route('/name').get(function(req, res){
+  res.json({ name: req.query.first + ' ' + req.query.last });
+}).post();
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
